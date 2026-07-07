604b98331e6d864660f178d6ee7f993bab0d3a25
rabiatul-starcantika <rabiatulakzaa@gmail.com>
2026-07-07 14:12:35 +0700

    saya akan mendemo ci cd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
             <div class="col-lg-6">
 
                 <h5 class="text-danger fw-bold">
-                    SELAMAT DATANG DI DIMSUM JAJANAN RUMAHAN
+                    SELAMAT DATANG DI SECUKUP RASA
                 </h5>
 
                 <h1 class="display-4 fw-bold">
